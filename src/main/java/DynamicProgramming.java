c5bec601bc8253e4dc059733eb2f59dc29fbfa55
guizhenyu <guizy@deepblueai.com>
2019-11-28 11:13:04 +0800

    Initial commit

---
+++ src/main/java/DynamicProgramming.java
@@ -0,0 +1,28 @@
+/**
+ * description: DynamicProgramming 动态规划
+ * date: 2019/11/27 7:01 下午
+ * author: guizhenyu
+ */
+public class DynamicProgramming {
+
+
+    static int getValue(int[] values, int rodLength) {
+        int[] subSolutions = new int[rodLength + 1];
+
+        for (int i = 1; i <= rodLength; i++) {
+            int tmpMax = -1;
+            for (int j = 0; j < i; j++)
+                tmpMax = Math.max(tmpMax, values[j] + subSolutions[i - j - 1]);
+            subSolutions[i] = tmpMax;
+        }
+
+        return subSolutions[rodLength];
+    }
+
+    public static void main(String[] args) {
+        int[] values = new int[]{3, 7, 1, 3, 9};
+        int rodLength = values.length;
+
+        System.out.println("Max rod value: " + getValue(values, rodLength));
+    }
+}
---
+++ src/main/java/RecurrenceFunction.java
@@ -0,0 +1,33 @@
+/**
+ * description: RecurrenceFunction 递归函数
+ * date: 2019/11/27 10:54 上午
+ * author: guizhenyu
+ */
+public class RecurrenceFunction {
+
+    static int getValue(int[] values, int length) {
+        if (length <= 0)
+            return 0;
+        int tmpMax = -1;
+        for (int i = 0; i < length; i++) {
+            System.out.println("i:" + i);
+            System.out.println("values[i]:" + values[i]);
+            tmpMax = Math.max(tmpMax, values[i] + getValue(values, length - i - 1));
+            System.out.println("inner length: " + length);
+            System.out.println("inner tmpMax: " + tmpMax);
+        }
+        System.out.println("length ===========================:" + length);
+
+        System.out.println(tmpMax);
+        return tmpMax;
+    }
+
+    public static void main(String[] args) {
+        int[] values = new int[]{1, 2, 6, 4, 5};
+        int rodLength = values.length;
+        System.out.println("Max rod value: " + getValue(values, rodLength));
+    }
+
+
+
+}
